063b00cb1cefe6a32d655124d3ec46d50d43e325
Joshua Booth <joshua.n.booth@gmail.com>
2022-12-14 10:22:09 -0800

    update

--- gen.sh
+++ gen.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 input="tags.txt"
 output="tags.css"
-echo ".tag-house\+of\+leaves:after { content: none !important }" >> $output
+echo -n ".tag-house\\%20of\\%20leaves:after{content:none!important}" > $output
 while read -r line
 do
   name="${line%%: *}"
-  name="${name//[ -]/\\+}"
+  name="${name//[ -]/\\%20}"
   name="${name//\'/\\%27}"
   name="${name//!/\\%21}"
   desc="${line#*: }"
   desc="${desc//\"/\\\"}"
   desc="${desc//./}"
-  echo ".tag-$name:after { content: \"$desc\" !important }" >> $output
+  echo -n ".tag-$name:after{content:\"$desc\"!important}" >> $output
 done < $input
